c7bab56b40498b5ea2916f2351f72e5835a436c7
matthewmaribojoc <matthewmaribojoc@gmail.com>
2019-11-10 23:01:57 -0500

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -18,10 +18,22 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: () => import("../views/PageNotFound.vue")
+  },
+  {
+    path: "/:productPath",
+    name: "Product",
+    props: true,
+    component: () => import("../views/Product.vue")
   }
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes
 });
 
